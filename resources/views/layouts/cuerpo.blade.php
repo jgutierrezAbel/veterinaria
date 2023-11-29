cbfd5ae94b2903c069e85811150ee6dc1491a8ee
RGalindo2004 <151685550+RGalindo2004@users.noreply.github.com>
2023-11-29 00:45:10 -0600

    'subiendo'

--- resources/views/layouts/cuerpo.blade.php
+++ resources/views/layouts/cuerpo.blade.php
@@ -13,21 +13,17 @@
   <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a href="/portada" class="navbar-brand">Inicio</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a href="/portada" class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
+      
+        
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Menú
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <li><a href="/proveedores" class="dropdown-item" href="#">Proveedores</a></li>
@@ -35,10 +31,17 @@
             <li><a href="/departamento" class="dropdown-item" href="#">Departamentos</a></li>
             <li><a href="/empleado" class="dropdown-item" href="#">Departamentos</a></li>
             <li><a href="/especialidades" class="dropdown-item" href="#">Especialidades</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a href="/portada/create" class="dropdown-item" href="#">Acerca de nosotros</a></li>
           </ul>
+          <li class="nav-item">
+          <a href="/portada/create" class="nav-link" href="#">Acerca de nosotros</a>
         </li>
+        </li>
+
+        <li class="nav-item">
+          <a href="/" class="nav-link active" aria-current="page" href="#">Cerrar Sesión</a>
+        </li>
+
+        
         <li class="nav-item">
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
         </li>
@@ -71,3 +74,4 @@
     -->
   </body>
 </html>
+
