5fd42d2ad17d973954b09af020c5bfdb525eb100
RGalindo2004 <151685550+RGalindo2004@users.noreply.github.com>
2023-11-30 00:18:21 -0600

    'subiendo'

---
+++ resources/views/mascotas/index.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.cuerpo')
+
+@section('hijos')
+
+
+
+<a href="empleado/create"class="btn btn-success">Crear</a>
+<table class="table table-striped table-hover">
+<h1>Lista de empleados</h1>
+
+    <table class ="table">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>nombre</th>
+                <th>apellido</th>
+                <th>fecha de naciemiento</th>
+                <th>género</th>
+                
+                <th>Departamento al que pertenece</th>
+                
+            </tr>
+
+        <tbody>
+        
+        @foreach ($emple as $emplea)
+            <tr>
+                <th>{{$emplea->id}}</th>
+                <th>{{$emplea->nombre}}</th>
+                <th>{{$emplea->apellido}}</th>
+                <th>{{$emplea->fecha_nac}}</th>
+                <th>{{$emplea->id_genero}}</th>
+                
+                <th>{{$emplea->departamento_nombre}}</th>
+            
+            <th>
+            <form action="/empleado/{{{$emplea->id}}}" method="post">
+                    @csrf
+                    @method('DELETE')
+
+                 <a href="/empleado/{{$emplea->id}}/edit"class="btn btn-primary">Editar</a>
+                 <a href="/empleado/{{$emplea->id}}" class="btn btn-danger">Eliminar</a>
+                 </form> 
+                </th>
+                </tr>
+                
+        @endforeach
+        
+        </tbody>
+
+
+
+
+        </thead>
+
+
+
+
+
+    </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
